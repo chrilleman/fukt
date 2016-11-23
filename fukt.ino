0b03dd29486f4a991357f0c5f8f969d5416f493c
chrilleman <christer.hoerlin@gmail.com>
2016-11-23 21:00:43 +0100

    Added display functionality

--- fukt.ino
+++ fukt.ino
@@ -2,7 +2,10 @@
 #include <U8glib.h>
 
 #define led13 13
+int counter = 0;
 U8GLIB_SSD1306_128X64 oled(U8G_I2C_OPT_NONE);
+
+
 void setup()
 {
   pinMode(led13, OUTPUT);
@@ -14,29 +17,28 @@ void loop()
   delay(1000);
   digitalWrite(led13,LOW);
   delay(500);
-
+  counter++;
   oled.firstPage();
   do
   {
-    UpdateDisplay(23.4,54.3);
+    UpdateDisplay(0.4 + counter, 54.3 - counter);
   }while(oled.nextPage());
 }
 
-void UpdateDisplay(double temp, double hum)
+void UpdateDisplay(float temp, float hum)
 {
+  char charBuf[50];
   oled.setFont(u8g_font_helvB10);
   oled.drawStr(0,12,"Temperature");
-  char charBuf[50];
   
-  oled.setFont(u8g_font_helvB18);
-  sprintf(charBuf, "%d degC",temp);
-  oled.drawStr(0,36,charBuf);
-  //str.toCharArray(charBuf,50);
-  //oled.drawLine(0,15,127,15);
-  
-  //char h[] = "0.0";
-  //oled.drawStr(0,32,"Humidity: ");
-  //oled.drawLine(0,35,127,35);
-  
-  
+  oled.setFont(u8g_font_helvB12);
+  dtostrf(temp,5,2,charBuf);
+  oled.drawStr(0,30,charBuf);
+
+  oled.setFont(u8g_font_helvB10);
+  oled.drawStr(0,44,"Humidity");
+
+  oled.setFont(u8g_font_helvB12);
+  dtostrf(hum,5,2,charBuf);
+  oled.drawStr(0,64,charBuf);
 }
